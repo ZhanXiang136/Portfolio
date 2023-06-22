86c6a9e3ddb271ec1ae5d75788b00bf24c732c14
Zhan Xiang Zheng <zhanxiangzheng136@gmail.com>
2023-06-21 23:37:50 -0400

    Basic landing css done

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         <header>
             <nav>
                 <ul>
-                    <li><a href="#landing">Home</a></li>
+                    <li><a href="#top">Home</a></li>
                     <li><a href="#about-me">About</a></li>
                     <li><a href="#portfolio">Portfolio</a></li>
                     <li><a href="#footer">Contact</a></li>
--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -7,7 +7,6 @@ export default function Landing() {
                 <div className="hero-content">
                     <h1>Welcome to my personal website!</h1>
                     <p>Here you'll find information about me, my work, and how to get in touch.</p>
-                    <button>Get in touch</button>
                 </div>
             </section>
         </section>
